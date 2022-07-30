e48e293795db92dd96da7848cdd7134101a7df94
Nouman Zahid <noumanzahid@hotmail.com>
2022-07-30 21:04:42 +0500

    BackEnd Url Changed

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
 // export const BackEndUrl = "https://inspiry-learning-backend.herokuapp.com";
-export const BackEndUrl = "http://localhost:8000"
+export const BackEndUrl = "https://inspirylearning-web.herokuapp.com"
