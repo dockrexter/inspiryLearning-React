64822016ec528a61cf5b3c5837cf28dcf61f2bdf
dockrexter <mshaheer.rahi@gmail.com>
2022-09-12 08:28:49 +0500

    new build

--- src/url.js
+++ src/url.js
@@ -4,4 +4,4 @@
 // export const BackEndUrl = "http://192.168.100.125:8000"
 //export const BackEndUrl = 'https://server.inspirylearning.com'
 //export const BackEndUrl = 'https://goofy-shirley.212-227-227-90.plesk.page'
-export const BackEndUrl = 'http://ec2-52-40-35-130.us-west-2.compute.amazonaws.com:8000'
+export const BackEndUrl = '/backend'
