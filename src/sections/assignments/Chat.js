2824076db982874577852bc02126563df52df785
dockrexter <mshaheer.rahi@gmail.com>
2022-09-13 01:11:16 +0500

    new build

--- src/sections/assignments/Chat.js
+++ src/sections/assignments/Chat.js
@@ -72,7 +72,10 @@ export default function Chat() {
     const [messageT, setMessageT] = useState([]);
     const { user } = useSelector(state => state.user);
     const { assignment } = useSelector(state => state.assignment);
-    const socket = io();
+    const socket = io("https://inspirylearning.com", {
+        autoConnect: false,
+        transports: ['websocket'],
+    });
     const socketRef = useRef(socket);
     const hiddenInputField = useRef(null);
     const [isFilePicked, setIsFilePicked] = useState(false);
