130e5f9c8cfa523acf8f7ec9422a6c01f1811c66
Nouman <noumanzahid@hotmail.com>
2022-10-04 12:55:42 +0500

    URL plus Build

--- src/sections/assignments/Chat.js
+++ src/sections/assignments/Chat.js
@@ -72,7 +72,7 @@ export default function Chat() {
     const [messageT, setMessageT] = useState([]);
     const { user } = useSelector(state => state.user);
     const { assignment } = useSelector(state => state.assignment);
-    const socket = io("http://localhost:8080", {
+    const socket = io("https://inspirylearning.com", {
         'path': '/newSocket',
     });
     const socketRef = useRef(socket);
